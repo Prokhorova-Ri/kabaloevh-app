73d95ea2efe97147c1a107fd7b92a10cffef225c
DarkLis <coolbiird@gmail.com>
2023-04-11 15:10:21 +0300

    Добавлены стили меню active

--- src/components/BottomMenu/index.vue
+++ src/components/BottomMenu/index.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="menu">
-    <router-link
+    <div
       v-for="(item, index) in menuList"
       :key="index"
-      :to="item.url"
-      :style="item.active ? 'color: black;' : 'color: white'"
-      class="menu-item"
-    >
-      <img class="menu-item__image menu-item__active" :src="item.icon" />
-      {{ item.title }}
-    </router-link>
+      :class="item.active ? 'menu-item__active' : ''">
+      <router-link
+        :to="item.url"
+        class="menu-item"
+      >
+        <img class="menu-item__image" :src="item.icon" />
+        {{ item.title }}
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -29,29 +31,33 @@ export default {
 
 <style lang="scss" scoped>
 .menu {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
   position: absolute;
   bottom: 0;
   left: 0;
   right: 0;
   width: 100%;
-  padding: 10px 13px;
-  display: flex;
-  justify-content: space-between;
   background: rgba(50, 149, 107, 0.8);
   &-item {
+    padding: 9px 10px;
     color: white;
-    font-size: 14px;
+    font-size: 12px;
     text-decoration: none;
     display: flex;
     flex-direction: column;
     align-items: center;
     row-gap: 6px;
     &__image {
-      width: 24px;
-      height: 24px;
+      width: 20px;
+      height: 20px;
     }
     &__active {
-      fill: black;
+      background-color: #FF7A00;
+      transition: all 4ms ease-in;
+      //-webkit-box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.2);
+      //-moz-box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.2);
+      //box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.2);
     }
   }
 }
