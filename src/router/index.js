91f93d013fe1edb0d9cb5c7fc0073d8c37ee612d
Prokhorova_Ri <rena2652@gmail.com>
2023-04-11 09:08:48 +0300

    FIRST COMMIT

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from "/src/views/Home.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2 class='home'>Приветствуем вас в приложении "Мобильный шиномонтаж"!</h2>
+  <form class='home_form'>
+    <h2 class='home_form__title'>Расчитать стоимость</h2>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang='scss' scoped>
+.home {
+  margin: 0 auto;
+  padding: 40px 17px 0 17px;
+  display: flex;
+  text-align: center;
+  &_form {
+    background-color: rgba(50, 149, 107, 0.8);
+    border-radius: 10px;
+    height: 300px;
+    width: 340px;
+  }
+}
+
+</style>
