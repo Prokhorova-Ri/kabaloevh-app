323e89ab79c7f27be313d2118aef8ae7d19ca064
Prokhorova_Ri <rena2652@gmail.com>
2023-04-14 15:13:24 +0300

    WRAPPER LAYOUT

---
+++ src/components/Universal/WrapperLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class='wrapper-layout'
+    :style="'background: #green'"
+  >
+
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'WrapperLayout',
+  setup () {
+
+    return {}
+  }
+})
+</script>
+
+<style lang='scss' scoped>
+  .wrapper-layout {}
+</style>
--- src/views/price/index.vue
+++ src/views/price/index.vue
@@ -1,15 +1,17 @@
 <template>
-  <div>
-    TODO LAYOUT PRICE
+  <div class='price'>
+    <h1 class='price-title'>Прайс</h1>
+    <WrapperLayout />
     <Button text='Оставить заявку' @onClickButton='test'/>
   </div>
 </template>
 
 <script>
 import Button from '../../components/Universal/Button.vue'
+import WrapperLayout from '../../components/Universal/WrapperLayout.vue'
 export default {
   name: 'index',
-  components: { Button },
+  components: { WrapperLayout, Button },
   setup() {
 
     const test = () => {
