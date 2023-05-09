3bee34d59766afd6f87accb299e32392e6e29eea
IlyaLesunov <coolbiird@gmail.com>
2023-05-09 15:32:13 +0300

    Добавление рейтинга через звездочку

--- src/components/Universal/Inputs/SimpleRating.vue
+++ src/components/Universal/Inputs/SimpleRating.vue
@@ -1,40 +1,15 @@
 <template>
-  <div>
-    <img
-      v-for='(index) in starsCount'
-      :key='index'
-      src='src/assets/images/icons/star.png'
-      @click.stop='setStarsAnswer(index)'
-    />
-    <img
-      v-for='(index) in selectStarCount'
-      :key='index'
-      src='src/assets/images/icons/star-full.png'
-      @click.stop='setStarsAnswer(index)'
-    />
-<!--    <img-->
-<!--      v-for='(index) in starDefault'-->
-<!--      :key='index'-->
-<!--      src='src/assets/images/icons/star.png'>-->
+  <div style='display: flex; gap: 9px; position: relative'>
+    <div
+      ref='listStar'
+      v-for='(item) in schema.max'
+      :key='item'
+      class='simple-rating__simple'
+      @click='setStarCount(item)'
+    >
+      <img src='src/assets/images/icons/star.png' />
+    </div>
   </div>
-<!--  <div class='simple-rating'>-->
-<!--   <div class='simple-rating__items'>-->
-<!--     <div v-for='(item, index) in starView' :key='index'>-->
-<!--       <input-->
-<!--         :id='`simple-rating-${item}`'-->
-<!--         type='radio'-->
-<!--         :value='item'-->
-<!--         class='simple-rating__item'-->
-<!--         name='simple-rating'-->
-<!--       >-->
-<!--       <label-->
-<!--         :for='`simple-rating-${item}`'-->
-<!--         class='simple-rating__label'-->
-<!--       >-->
-<!--       </label>-->
-<!--     </div>-->
-<!--   </div>-->
-<!--  </div>-->
 </template>
 
 <script>
@@ -50,76 +25,48 @@ export default {
     }
   },
   setup(props) {
+    const listStar = ref()
 
-    const starsCount = ref(props?.schema?.max)
-    const selectStarCount = ref(props?.schema?.answer)
-
-    const setStarsAnswer = (index) => {
-      selectStarCount.value = index
-      props.schema.answer = index
-      console.log('starsCount', starsCount.value)
+    const setStarCount = (count) => {
+      listStar.value.forEach((item, index) => {
+        if (count > index) {
+          item.classList.add('simple-rating__full')
+        } else {
+          item.classList.remove('simple-rating__full')
+        }
+      })
+      props.schema.answer = count || 0
+      console.log('props.schema', props.schema)
     }
 
-    console.log('starsCount', starsCount.value)
-
     return {
-      starsCount,
-      selectStarCount,
-      setStarsAnswer
+      listStar,
+      setStarCount
     }
   }
 }
 </script>
 
-<style lang='scss' scoped>
-  //.simple-rating {
-  //  position: relative;
-  //  font-size: 33px;
-  //  display: inline-block;
-  //  &:before {
-  //    content: "★★★★★";
-  //    display: block;
-  //  }
-  //  &__items {
-  //    position: absolute;
-  //    width: 100%;
-  //    height: 100%;
-  //    top: 0;
-  //    left: 0;
-  //    display: flex;
-  //    flex-direction: row-reverse;
-  //    overflow: hidden;
-  //  }
-  //  &__item {
-  //    position: absolute;
-  //    width: 0;
-  //    height: 0;
-  //    top: 0;
-  //    left: 0;
-  //    opacity: 0;
-  //    visibility: hidden;
-  //  }
-  //  &__label {
-  //    flex: 0 0 20%;
-  //    height: 100%;
-  //    cursor: pointer;
-  //    color: white;
-  //    &:before {
-  //      content: "★";
-  //      display: block;
-  //      transition: color 0.3s ease 0s;
-  //    }
-  //  }
-  //}
-  //.simple-rating__item:checked {
-  //  color: #F2CC6A;
-  //}
-  //.simple-rating__item:checked ~ .simple-rating__label {
-  //  color: #F2CC6A;
-  //}
-  //.simple-rating:hover,
-  //.simple-rating:hover ~ .simple-rating__label,
-  //.simple-rating:checked ~ .simple-rating__label:hover {
-  //  color: red;
-  //}
+<style lang='scss'>
+  .simple-rating {
+    &__full {
+      position: relative;
+      animation: active-bg-in .7s forwards;
+      &::after {
+        content: url("src/assets/images/icons/star-full.png");
+        position: absolute;
+        left: 0;
+        right: 0;
+      }
+    }
+  }
+
+  @keyframes active-bg-in {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
 </style>
